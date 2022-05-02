7cd84ebef34dc23115389aab17a0c34c4795921d
ErnestJo <smartlabstech@yahoo.com>
2022-05-02 09:55:46 +0300

    add meetup page and route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-
-import { Route, Switch } from 'react-router-dom';
-import AllMeetUp from './pages/AllmeetUps';
-import NewMeetUp from './pages/NewMeetsUps';
-import Favorites from './pages/Favorite';
-import MainNavigation from './components/layout/MainNavigation';
+import { Route, Switch } from "react-router-dom";
+import AllMeetUp from "./pages/AllmeetUps";
+import NewMeetUp from "./pages/NewMeetsUps";
+import Favorites from "./pages/Favorite";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route path='/' exact>
-          <AllMeetUp />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetUp />
-        </Route>
-        <Route path='/favorites'>
-          <Favorites />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <AllMeetUp />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetUp />
+          </Route>
+          <Route path="/favorites">
+            <Favorites />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
