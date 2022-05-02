f725e98354bc84a174cf883d89e66ea2bab90404
ErnestJo <smartlabstech@yahoo.com>
2022-05-02 09:04:28 +0300

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 
+import { Route, Switch } from 'react-router-dom';
+import AllMeetUp from './pages/AllmeetUps';
+import NewMeetUp from './pages/NewMeetsUps';
+import Favorites from './pages/Favorite';
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
   return (
-    <div >
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetUp />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetUp />
+        </Route>
+        <Route path='/favorites'>
+          <Favorites />
+        </Route>
+      </Switch>
     </div>
   );
 }
