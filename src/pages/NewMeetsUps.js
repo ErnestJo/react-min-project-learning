65eae1c4665e83a00b874f53fdd085530eda8880
ErnestJo <smartlabstech@yahoo.com>
2022-05-02 12:32:37 +0300

    favorite context

---
+++ src/pages/NewMeetsUps.js
@@ -0,0 +1,24 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
+function NewMeetUp() {
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch("https://meetup-e819e-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetUp;
